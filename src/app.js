4d5ebb83b6b313b21a8220eaaec6be9d91a43802
Javier Cubero <102825817+JaviCubero@users.noreply.github.com>
2025-06-24 19:10:50 +0000

    Script Java e interfaz HTML-CSS terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let end = ['.com', '.info', '.net', '.org', '.biz', '.edu'];
+
+  let domain = pronoun.flatMap(pronoun =>
+    adj.flatMap(adj =>
+      noun.flatMap(noun =>
+        end.map(end => `${pronoun}${adj}${noun}${end}`)
+      )
+    )
+  )
+
+  console.log(domain);
+
 };

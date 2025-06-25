b81358e5bf5f73f3aa1537ea4e0ff0630a8f3cde
Javier Cubero <102825817+JaviCubero@users.noreply.github.com>
2025-06-25 18:13:38 +0000

    Proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -13,8 +13,8 @@ window.onload = function () {
   let noun = ['jogger', 'racoon'];
   let end = ['.com', '.info', '.net', '.org', '.biz', '.edu'];
 
-  // Obtención de la variable "domain" que guardará en formato array todas las combinaciones de dominios existentes
-  let domain = pronoun.flatMap(pronoun =>
+  // Obtención de la variable "domainList" que guardará en formato array todas las combinaciones de dominios existentes
+  let domainList = pronoun.flatMap(pronoun =>
     adj.flatMap(adj =>
       noun.flatMap(noun =>
         end.map(end => `${pronoun}${adj}${noun}${end}`)
@@ -22,59 +22,63 @@ window.onload = function () {
     )
   )
 
-  
-  // A partir de aquí empieza el DOM
 
-  // Esta función saca por pantalla todos los dominios que tengan una terminación específica dentro de la variable "domain" donde están todas las combinaciones almacenadas
+  // Aquí comienza el DOM
+
+  // Esta función saca por pantalla todos los dominios que tengan una terminación específica dentro de la variable "domainList" donde están todas las combinaciones almacenadas
   function domainClicked(options) {
     let specificDomains = document.getElementById("specificDomains"); // Busca el elemento HTML donde se mostrará el texto en base al ID
-    specificDomains.innerHTML = ""; // Limpia contenido previo del elemento
+    specificDomains.innerHTML = ""; // Limpia el contenido previo del elemento
 
     let filteredDomains = [] // Esta variable almacena los dominios filtrados según su terminación
 
     switch (options) {
       case "com":
-        filteredDomains = document.createTextNode(
-          domain.filter(com => com.endsWith(".com")) // Se guarda en filteredDomains todos los dominios que tienen la terminación .com
-        );
+        filteredDomains = domainList.filter(com => com.endsWith(".com")); // Se guarda en filteredDomains todos los dominios que tienen la terminación .com
         break;
       case "inf":
-        filteredDomains = document.createTextNode(
-          domain.filter(inf => inf.endsWith(".info")) // Se guarda en filteredDomains todos los dominios que tienen la terminación .info
-        );
+        filteredDomains = domainList.filter(inf => inf.endsWith(".info")); // Se guarda en filteredDomains todos los dominios que tienen la terminación .info
         break;
       case "net":
-        filteredDomains = document.createTextNode(
-          domain.filter(net => net.endsWith(".net")) // Se guarda en filteredDomains todos los dominios que tienen la terminación .net
-        );
+        filteredDomains = domainList.filter(net => net.endsWith(".net")); // Se guarda en filteredDomains todos los dominios que tienen la terminación .net
         break;
       case "org":
-        filteredDomains = document.createTextNode(
-          domain.filter(org => org.endsWith(".org")) // Se guarda en filteredDomains todos los dominios que tienen la terminación .org
-        );
+        filteredDomains = domainList.filter(org => org.endsWith(".org")); // Se guarda en filteredDomains todos los dominios que tienen la terminación .org
         break;
       case "biz":
-        filteredDomains = document.createTextNode(
-          domain.filter(biz => biz.endsWith(".biz")) // Se guarda en filteredDomains todos los dominios que tienen la terminación .biz
-        );
+        filteredDomains = domainList.filter(biz => biz.endsWith(".biz")); // Se guarda en filteredDomains todos los dominios que tienen la terminación .biz
         break;
       case "edu":
-        filteredDomains = document.createTextNode(
-          domain.filter(edu => edu.endsWith(".edu")) // Se guarda en filteredDomains todos los dominios que tienen la terminación .edu
-        );
+        filteredDomains = domainList.filter(edu => edu.endsWith(".edu")); // Se guarda en filteredDomains todos los dominios que tienen la terminación .edu
         break;
     }
 
-    let newP = document.createElement("p"); // Crea un párrafo nuevo
-    newP.appendChild(filteredDomains);  // Añade el texto almacenado en filteredDomain al párrafo creado  
-    specificDomains.appendChild(newP); // Añade el párrafo creado al elemento HTML buscado al inicio de la función
+    // Crea un h5 a modo de subtítulo
+    let newH = document.createElement("h5");
+    newH.textContent = "Los dominios correspondientes a esta terminación son:";
+
+    // Crea una lista para mostrar los dominios de forma ordenada
+    let newList = document.createElement("ul");
+    newList.classList.add("domain-list"); // Crea una clase para poder modificar el estilo de la nueva lista desde styless.css
+
+    // Esta función crea un li para cada dominio dentro de la lista ul creada
+    filteredDomains.forEach((domain) => {
+      let listItem = document.createElement("li");
+      listItem.textContent = domain;
+      newList.appendChild(listItem);
+    });
+
+    // Añade el h5 y la lista creada al elemento HTML buscado al inicio de la función
+    specificDomains.appendChild(newH);
+    specificDomains.appendChild(newList);
   }
 
-  let buttons = document.querySelectorAll("button[domain-option]"); // Busca todos los botones HTML que tengan el atributo "domain-option"
+  // Busca todos los botones HTML que tengan el atributo "domain-option"
+  let buttons = document.querySelectorAll("button[domain-option]");
 
-  buttons.forEach(button => button.addEventListener('click', () => { // Espera a que se haga click en alguno de los botones para ejecutar la función
+  // Esta función espera a que se haga click en alguno de los botones para ser ejecutada
+  buttons.forEach(button => button.addEventListener('click', () => {
     let domainOption = button.getAttribute("domain-option"); // Se guarda el valor del botón que ha sido clickado en la variable domainOption
     domainClicked(domainOption); // Ejecuta la función domainClicked pasándole el valor del botón que se ha sido presionado
   }));
-
 };
